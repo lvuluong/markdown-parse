2ed775f326263ae15c74cd25d4b301bfc1e1a583
lvuluong <m2luong@ucsd.edu>
2022-03-11 01:06:41 -0800

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -12,18 +12,24 @@ public class MarkdownParse {
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
             int nextOpenBracket = markdown.indexOf("[", currentIndex);
-            if(nextOpenBracket == -1){
-                return null;
-            }
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             int openParen = markdown.indexOf("(", nextCloseBracket);
-            if(openParen == -1){
-                return null;
-            }
             int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
-            currentIndex = closeParen + 1;
-            //System.out.println("Current Index: " + currentIndex);
+            if(nextOpenBracket == -1 || nextCloseBracket == -1 
+                || openParen == -1 || closeParen == -1){
+                break;
+            }
+            if(openParen == nextCloseBracket + 1){
+                if(nextOpenBracket != 0){
+                    char checkImage = markdown.charAt(nextOpenBracket - 1);
+                    if(!String.valueOf(checkImage).equals("!")){
+                        toReturn.add(markdown.substring(openParen + 1, closeParen).trim());
+                    }
+                }else{
+                    toReturn.add(markdown.substring(openParen + 1, closeParen).trim());
+                }
+            }
+            currentIndex = nextCloseBracket + 1;
         }
         return toReturn;
     }
